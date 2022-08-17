536b3b9dedab964ef934dfe121a6d7338872954c
Skyler Burwell <skyboy49707@gmail.com>
2022-08-16 23:20:58 -0600

    Start.

---
+++ src/token.cpp
@@ -0,0 +1,504 @@
+/*
+ * Nifty - Nifty Programming Language
+ * Copyright (c) 2022 Skyler Burwell
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ */
+
+#include "token.hpp"
+
+namespace {
+    String fmtTTS(const String &type, const String &lexeme) {
+        return "<" + type + ", '" + lexeme + "'>";
+    }
+}
+
+std::map<String, Tokens> Token::keywords = {
+        {"let", TK_LET},
+        {"val", TK_VAL},
+        {"const", TK_CONST},
+        {"new", TK_NEW},
+        {"delete", TK_DELETE},
+        {"global", TK_GLOBAL},
+        {"local", TK_LOCAL},
+
+        {"fn", TK_FN},
+
+        {"return", TK_RETURN},
+        {"if", TK_IF},
+        {"else", TK_ELSE},
+        {"elif", TK_ELIF},
+        {"do", TK_DO},
+        {"in", TK_IN},
+        {"step", TK_STEP},
+        {"for", TK_FOR},
+        {"forever", TK_FOREVER},
+        {"while", TK_WHILE},
+        {"until", TK_UNTIL},
+        {"repeat", TK_REPEAT},
+        {"when", TK_WHEN},
+        {"break", TK_BREAK},
+        {"continue", TK_CONTINUE},
+        {"goto", TK_GOTO},
+        {"delay", TK_DELAY},
+        {"default", TK_DEFAULT},
+
+        {"type", TK_TYPE},
+        {"as", TK_AS},
+
+        {"class", TK_CLASS},
+        {"struct", TK_STRUCT},
+        {"enum", TK_ENUM},
+        {"union", TK_UNION},
+        {"self", TK_SELF},
+        {"public", TK_PUBLIC},
+        {"private", TK_PRIVATE},
+        {"read_only", TK_READ_ONLY},
+        {"protected", TK_PROTECTED},
+        {"friend", TK_FRIEND},
+
+        {"assert", TK_ASSERT},
+        {"operator", TK_OPERATOR},
+
+        {"and", TK_AND},
+        {"or", TK_OR},
+        {"true", TK_TRUE},
+        {"false", TK_FALSE},
+
+        {"sizeof", TK_SIZEOF},
+        {"alignof", TK_ALIGNOF},
+        {"typeof", TK_TYPEOF},
+        {"nameof", TK_NAMEOF},
+
+        {"int", TK_INT},
+        {"float", TK_FLOAT},
+        {"double", TK_DOUBLE},
+        {"string", TK_STRING_TYPE},
+        {"char", TK_CHAR_TYPE},
+        {"bool", TK_BOOL},
+        {"uint8", TK_U8},
+        {"uint16", TK_U16},
+        {"uint32", TK_U32},
+        {"uint64", TK_U64},
+        {"uint128", TK_U128},
+        {"int8", TK_S8},
+        {"int16", TK_S16},
+        {"int32", TK_S32},
+        {"int64", TK_S64},
+        {"int128", TK_S128},
+        {"isize", TK_ISIZE},
+        {"usize", TK_USIZE},
+        {"void", TK_VOID},
+        {"null", TK_NULL},
+
+        {"package", TK_PACKAGE},
+        {"use", TK_USE},
+
+        {"asm", TK_ASM},
+        {"inline", TK_INLINE},
+};
+
+String tokenToString(const Token &t) {
+    if (t.lexeme.empty()) {
+        return "null";
+    }
+
+    var it = Token::keywords.find(t.lexeme);
+
+    if (it != Token::keywords.end()) {
+        return fmtTTS("TK_" + t.lexeme.toUpper(), t.lexeme);
+    }
+
+    switch (t.type) {
+        case TK_UNKNOWN: {
+            return fmtTTS("TK_UNKNOWN", t.lexeme);
+        }
+        case TK_COLON: {
+            return fmtTTS("TK_COLON", t.lexeme);
+        }
+        case TK_COMMA: {
+            return fmtTTS("TK_COMMA", t.lexeme);
+        }
+        case TK_SEMICOLON: {
+            return fmtTTS("TK_SEMICOLON", t.lexeme);
+        }
+        case TK_LPAREN: {
+            return fmtTTS("TK_LPAREN", t.lexeme);
+        }
+        case TK_RPAREN: {
+            return fmtTTS("TK_RPAREN", t.lexeme);
+        }
+        case TK_LBRACE: {
+            return fmtTTS("TK_LBRACE", t.lexeme);
+        }
+        case TK_RBRACE: {
+            return fmtTTS("TK_RBRACE", t.lexeme);
+        }
+        case TK_LBRACKET: {
+            return fmtTTS("TK_LBRACKET", t.lexeme);
+        }
+        case TK_RBRACKET: {
+            return fmtTTS("TK_RBRACKET", t.lexeme);
+        }
+        case TK_DOT: {
+            return fmtTTS("TK_DOT", t.lexeme);
+        }
+        case TK_IRANGE: {
+            return fmtTTS("TK_IRANGE", t.lexeme);
+        }
+        case TK_ERANGE: {
+            return fmtTTS("TK_ERANGE", t.lexeme);
+        }
+        case TK_VARY: {
+            return fmtTTS("TK_VARY", t.lexeme);
+        }
+        case TK_ANNOTATION: {
+            return fmtTTS("TK_ANNOTATION", t.lexeme);
+        }
+        case TK_MACRO: {
+            return fmtTTS("TK_MACRO", t.lexeme);
+        }
+
+        case TK_ASSIGN: {
+            return fmtTTS("TK_ASSIGN", t.lexeme);
+        }
+        case TK_EQU: {
+            return fmtTTS("TK_EQU", t.lexeme);
+        }
+        case TK_ADD: {
+            return fmtTTS("TK_ADD", t.lexeme);
+        }
+        case TK_INC: {
+            return fmtTTS("TK_INC", t.lexeme);
+        }
+        case TK_ADDEQU: {
+            return fmtTTS("TK_ADDEQU", t.lexeme);
+        }
+        case TK_SUB: {
+            return fmtTTS("TK_SUB", t.lexeme);
+        }
+        case TK_DEC: {
+            return fmtTTS("TK_DEC", t.lexeme);
+        }
+        case TK_SUBEQU: {
+            return fmtTTS("TK_SUBEQU", t.lexeme);
+        }
+        case TK_MUL: {
+            return fmtTTS("TK_MUL", t.lexeme);
+        }
+        case TK_MULEQU: {
+            return fmtTTS("TK_MULEQU", t.lexeme);
+        }
+        case TK_DIV: {
+            return fmtTTS("TK_DIV", t.lexeme);
+        }
+        case TK_DIVEQU: {
+            return fmtTTS("TK_DIVEQU", t.lexeme);
+        }
+        case TK_MOD: {
+            return fmtTTS("TK_MOD", t.lexeme);
+        }
+        case TK_MODEQU: {
+            return fmtTTS("TK_MODEQU", t.lexeme);
+        }
+        case TK_GREATER: {
+            return fmtTTS("TK_GREATER", t.lexeme);
+        }
+        case TK_GREATEREQU: {
+            return fmtTTS("TK_GREATEREQU", t.lexeme);
+        }
+        case TK_LESS: {
+            return fmtTTS("TK_LESS", t.lexeme);
+        }
+        case TK_LESSEQU: {
+            return fmtTTS("TK_LESSEQU", t.lexeme);
+        }
+
+        case TK_IDENT: {
+            return fmtTTS("TK_IDENT", t.lexeme);
+        }
+        case TK_STRING_LIT: {
+            return fmtTTS("TK_STRING_LIT", t.lexeme);
+        }
+        case TK_CHAR_LIT:{
+            return fmtTTS("TK_CHAR_LIT", t.lexeme);
+        }
+        case TK_NUMBER: {
+            return fmtTTS("TK_NUMBER", t.lexeme);
+        }
+
+        case TK_QMRK: {
+            return fmtTTS("TK_QMRK", t.lexeme);
+        }
+        case TK_QDOT: {
+            return fmtTTS("TK_QDOT", t.lexeme);
+        }
+        case TK_NULL_COALESCE: {
+            return fmtTTS("TK_NULL_COALESCE", t.lexeme);
+        }
+        case TK_NULL_COALESCE_ASSIGN: {
+            return fmtTTS("TK_NULL_COALESCE_ASSIGN", t.lexeme);
+        }
+
+        case TK_EOF: {
+            return fmtTTS("TK_EOF", t.lexeme);
+        }
+
+        default: {
+            return fmtTTS("???", t.lexeme);
+        }
+    }
+}
+
+bool isTokenType(int type) {
+    switch (type) {
+        case TK_INT:
+        case TK_FLOAT:
+        case TK_DOUBLE:
+        case TK_STRING_TYPE:
+        case TK_CHAR_TYPE:
+        case TK_BOOL:
+        case TK_U8:
+        case TK_U16:
+        case TK_U32:
+        case TK_U64:
+        case TK_U128:
+        case TK_S8:
+        case TK_S16:
+        case TK_S32:
+        case TK_S64:
+        case TK_S128: {
+            return true;
+        }
+
+        default: {
+            return false;
+        }
+    }
+}
+
+bool isTokenOp(int type) {
+    switch (type) {
+        case TK_ADD:
+        case TK_ADDEQU:
+        case TK_SUB:
+        case TK_SUBEQU:
+        case TK_MUL:
+        case TK_MULEQU:
+        case TK_DIV:
+        case TK_DIVEQU:
+        case TK_MOD:
+        case TK_MODEQU:
+        case TK_INC:
+        case TK_DEC:
+        case TK_ASSIGN:
+        case TK_EQU:
+        case TK_POS:
+        case TK_NEG:
+        case TK_NOT:
+        case TK_NOTEQU:
+        case TK_LESS:
+        case TK_LESSEQU:
+        case TK_GREATER:
+        case TK_GREATEREQU:
+        case TK_AND:
+        case TK_OR:
+        case TK_BITAND:
+        case TK_BITOR: {
+            return true;
+        }
+
+        default: {
+            return false;
+        }
+    }
+}
+
+bool isTokenBinOp(int type) {
+    switch (type) {
+        case TK_ADD:
+        case TK_ADDEQU:
+        case TK_SUB:
+        case TK_SUBEQU:
+        case TK_MUL:
+        case TK_MULEQU:
+        case TK_DIV:
+        case TK_DIVEQU:
+        case TK_MOD:
+        case TK_MODEQU:
+        case TK_ASSIGN:
+        case TK_EQU:
+        case TK_NOTEQU:
+        case TK_LESS:
+        case TK_LESSEQU:
+        case TK_GREATER:
+        case TK_GREATEREQU:
+        case TK_AND:
+        case TK_OR:
+        case TK_BITAND:
+        case TK_BITOR: {
+            return true;
+        }
+
+        default: {
+            return false;
+        }
+    }
+}
+
+bool isTokenOpPrePostFix(int type) {
+    switch (type) {
+        case TK_INC:
+        case TK_DEC: {
+            return true;
+        }
+
+        default: {
+            return false;
+        }
+    }
+}
+
+bool isTokenUnaryOp(int type) {
+    switch (type) {
+        case TK_POS:
+        case TK_NEG:
+        case TK_INC:
+        case TK_DEC:
+        case TK_NOT: {
+            return true;
+        }
+
+        default: {
+            return false;
+        }
+    }
+}
+
+int getTokenPrecedence(int type) {
+    switch (type) {
+        case TK_INC:
+        case TK_DEC: {
+            return 100;
+        }
+
+        case TK_PREINC:
+        case TK_PREDEC:
+        case TK_NOT:
+        case TK_BITNOT: {
+            return 90;
+        }
+
+        case TK_MUL:
+        case TK_DIV:
+        case TK_MOD: {
+            return 80;
+        }
+
+        case TK_ADD:
+        case TK_SUB: {
+            return 70;
+        }
+
+        case TK_LESS:
+        case TK_LESSEQU:
+        case TK_GREATER:
+        case TK_GREATEREQU: {
+            return 60;
+        }
+
+        case TK_EQU:
+        case TK_NOTEQU: {
+            return 50;
+        }
+
+        case TK_BITAND: {
+            return 40;
+        }
+
+        case TK_BITOR: {
+            return 30;
+        }
+
+        case TK_AND: {
+            return 20;
+        }
+
+        case TK_OR: {
+            return 10;
+        }
+
+        case TK_ASSIGN:
+        case TK_ADDEQU:
+        case TK_SUBEQU:
+        case TK_MULEQU:
+        case TK_DIVEQU:
+        case TK_MODEQU: {
+            return 9;
+        }
+
+        case TK_COMMA: {
+            return 1;
+        }
+
+        default: {
+            return -1;
+        }
+    }
+}
+
+int getNumBitsForToken(int type) {
+    switch (type) {
+        case TK_S8:
+        case TK_U8:
+            return 8;
+        case TK_S16:
+        case TK_U16:
+            return 16;
+        case TK_S32:
+        case TK_U32:
+        case TK_INT:
+        case TK_FLOAT:
+        case TK_CHAR_TYPE:
+            return 32;
+        case TK_S64:
+        case TK_U64:
+        case TK_DOUBLE:
+            return 64;
+        case TK_S128:
+        case TK_U128:
+            return 128;
+        default:
+            return -1;
+    }
+}
+
+bool isSignedForToken(int type) {
+    switch (type) {
+        case TK_U8:
+        case TK_U16:
+        case TK_U32:
+        case TK_U64:
+        case TK_U128:
+        case TK_CHAR_TYPE:
+            return false;
+        default:
+            return true;
+    }
+}
+
---
+++ src/token.hpp
@@ -0,0 +1,219 @@
+/*
+ * Nifty - Nifty Programming Language
+ * Copyright (c) 2022 Skyler Burwell
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ */
+
+#ifndef __NIFTY_TOKEN_HPP__
+#define __NIFTY_TOKEN_HPP__
+
+#include <map>
+
+#include "util/str.hpp"
+
+enum Tokens {
+    TK_UNKNOWN = 0,
+
+    TK_FN,
+
+    TK_IF,
+    TK_IF_NOT,
+    TK_ELIF,
+    TK_ELSE,
+    TK_DO,
+    TK_IN,
+    TK_STEP,
+    TK_AS,
+    TK_CLASS,
+    TK_STRUCT,
+    TK_ENUM,
+    TK_UNION,
+    TK_SELF,
+    TK_PUBLIC,
+    TK_PRIVATE,
+    TK_READ_ONLY,
+    TK_PROTECTED,
+    TK_FRIEND,
+
+    TK_DELAY,
+    TK_ASSERT,
+
+    TK_TYPE,
+    TK_FOR,
+    TK_FOREVER,
+    TK_WHILE,
+    TK_UNTIL,
+    TK_WHEN,
+    TK_REPEAT,
+    TK_GOTO,
+    TK_BREAK,
+    TK_CONTINUE,
+    TK_RETURN,
+    TK_DEFAULT,
+
+    TK_ASM,
+    TK_INLINE,
+
+    TK_TRUE,
+    TK_FALSE,
+
+    TK_GLOBAL,
+    TK_LOCAL,
+    TK_LET, // mutable
+    TK_VAL, // immutable
+    TK_CONST, // immutable, compile time
+    TK_NEW,
+    TK_DELETE,
+
+    TK_USE,
+    TK_PACKAGE,
+
+    TK_SIZEOF,
+    TK_ALIGNOF,
+    TK_TYPEOF,
+    TK_NAMEOF,
+
+    TK_INT,
+    TK_FLOAT,
+    TK_DOUBLE,
+    TK_STRING_TYPE,
+    TK_CHAR_TYPE,
+    TK_BOOL,
+    TK_U8,
+    TK_U16,
+    TK_U32,
+    TK_U64,
+    TK_U128,
+    TK_S8,
+    TK_S16,
+    TK_S32,
+    TK_S64,
+    TK_S128,
+    TK_ISIZE,
+    TK_USIZE,
+    TK_VOID,
+    TK_NULL,
+
+    TK_OPERATOR,
+
+    TK_COLON,                   // :
+    TK_COMMA,                   // ,
+    TK_SEMICOLON,               // ;
+    TK_LPAREN,                  // (
+    TK_RPAREN,                  // )
+    TK_LBRACE,                  // {
+    TK_RBRACE,                  // }
+    TK_LBRACKET,                // [
+    TK_RBRACKET,                // ]
+    TK_DOT,                     // .
+    TK_IRANGE,                  // ..
+    TK_ERANGE,                  // ..<
+    TK_VARY,                    // ...
+    TK_LABEL,                   // ident:
+    TK_ANNOTATION,              // @
+    TK_MACRO,                   // #
+    TK_SCOPE,                   // ::
+    TK_LET_DECL,                // :=
+    TK_VAL_DECL,                // ::=
+
+    TK_ASSIGN,                  // =
+    TK_EQU,                     // == is
+    TK_AND,						// && and
+    TK_OR,						// || or
+    TK_NOT,						// ! not
+    TK_NOTEQU,					// !=
+    TK_ADD,                     // +
+    TK_INC,                     // x++
+    TK_PREINC,                  // ++x
+    TK_ADDEQU,                  // +=
+    TK_SUB,                     // -
+    TK_DEC,                     // x--
+    TK_PREDEC,                  // --x
+    TK_SUBEQU,                  // -=
+    TK_MUL,                     // *
+    TK_MULEQU,                  // *=
+    TK_DIV,                     // /
+    TK_DIVEQU,                  // /=
+    TK_MOD,                     // %
+    TK_MODEQU,                  // %=
+
+    TK_POS,                     // +x
+    TK_NEG,                     // -x
+
+    TK_GREATER,					// >
+    TK_GREATEREQU,				// >=
+    TK_LESS,					// <
+    TK_LESSEQU,					// <=
+
+    TK_XOR,                     // ^
+    TK_BITNOT,                  // ~
+    TK_BITAND,                  // &
+    TK_BITOR,                   // |
+    TK_BITLS,                   // <<
+    TK_BITRS,                   // >>
+    TK_XOREQU,                  // ^=
+    TK_BITANDEQU,               // &=
+    TK_BITOREQU,                // |=
+    TK_BITLSEQU,                // <<=
+    TK_BITRSEQU,                // >>=
+
+    TK_ADDR,                    // &x
+    TK_DEREF,                   // x^
+    TK_POINT,                   // ->
+    TK_PDREF,                   // ->*
+    TK_DOTREF,                  // .*
+
+    TK_QMRK,                    // ?
+    TK_QDOT,                    // ?.
+    TK_NULL_COALESCE,           // ??
+    TK_NULL_COALESCE_ASSIGN,    // ??=
+    TK_NULLISH_COALESCE,        // ||
+    TK_NULLISH_COALESCE_ASSIGN, // ||=
+
+    TK_IDENT,
+    TK_STRING_LIT,
+    TK_CHAR_LIT,
+    TK_NUMBER,
+
+    TK_EOF
+};
+
+struct Token {
+    String lexeme;
+    int type;
+    uint32 line;
+    uint32 pos;
+    uint32 index;
+
+    Token &operator=(const Token &other) = default;
+
+    static std::map<String, Tokens> keywords;
+};
+
+String tokenToString(const Token &t);
+bool isTokenType(int type);
+bool isTokenOp(int type);
+bool isTokenBinOp(int type);
+bool isTokenOpPrePostFix(int type);
+bool isTokenUnaryOp(int type);
+int getTokenPrecedence(int type);
+int getNumBitsForToken(int type);
+bool isSignedForToken(int type);
+
+#endif //__NIFTY_TOKEN_HPP__
